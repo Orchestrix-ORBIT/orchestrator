43681a25f7dfb5b15fb7c12b06d3ccdee143f3ea
Shehara Karunarathna <sheharakuni15@gmail.com>
2026-07-23 01:11:51 +0530

    The way database should be initialized in first docker run is implemented. All the previelleges for the backend user are granted

---
+++ backend/init-db.sql
@@ -0,0 +1,17 @@
+CREATE SCHEMA IF NOT EXISTS public; 
+
+CREATE ROLE orchestrix_app WITH LOGIN PASSWORD '${DB_PASSWORD}'; -- Created a new user for spring boot backend since root user should be available as an administrator
+
+--giving previllege to the backend user
+GRANT CONNECT ON DATABASE orchestrator_db TO orchestrix_app; -- Allows the user to connect to the database
+GRANT USAGE ON SCHEMA public TO orchestrix_app;
+GRANT CREATE ON SCHEMA public TO orchestrix_app; -- Allows the user to create tables in the database
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO orchestrix_app;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO orchestrix_app;
+
+GRANT CREATE ON DATABASE orchestrator_db TO orchestrix_app; -- Allows the user to create schemas in the database
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+    GRANT ALL ON TABLES TO orchestrix_app;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+    GRANT ALL ON SEQUENCES TO orchestrix_app;
